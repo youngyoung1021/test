5bd8ceff24f602342072afa6f6d1580206489b2b
youngyoung1021 <bebeqtq1995@gmail.com>
2019-10-31 19:15:54 +0900

    report card:Show the sum of each grade

---
+++ commit5/report_card.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(){
+	printf("This is Report Cards.c\n");
+	printf("what is the difference ? \n");
+	printf("Math: 90 \n");
+	printf("English: 100 \n");
+	printf("Sum grade: 190 \n");
+}
